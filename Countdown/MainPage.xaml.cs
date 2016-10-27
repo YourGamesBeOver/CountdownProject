7691d6d97c03d933c2b5da7d10476c5779c499a5
Coursin Hill <cgh28@case.edu>
2016-10-27 15:44:20 -0400

    Added in Top bar with hamburger button, name, and search bar. Added in side bar with calendar view, list view, and settings and sample frames for the 3 options.

--- Countdown/MainPage.xaml.cs
+++ Countdown/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Countdown;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,58 @@ namespace Countdown
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        List<HamOption> hamOptions = new List<HamOption>
+            {
+                new HamOption() { Title = "Profile" },
+                new HamOption() { Title = "Blog Posts" },
+                new HamOption() { Title = "Stats" },
+                new HamOption() { Title = "Settings" },
+
+            };
+
         public MainPage()
         {
             this.InitializeComponent();
+            MyFrame.Navigate(typeof(ListViewer));
         }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            mySplitView.IsPaneOpen = !mySplitView.IsPaneOpen;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyListBox.SelectedItem = null;
+            MyFrame.Navigate(typeof(ListViewer));
+        }
+
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ListViewListBoxItem.IsSelected)
+            {
+                MyFrame.Navigate(typeof(ListViewer));
+            }
+            if(CalendarViewListBoxItem.IsSelected)
+            {
+                MyFrame.Navigate(typeof(CalendarViewer));
+            }
+            else if(SettingsListBoxItem.IsSelected)
+            {
+                MyFrame.Navigate(typeof(SettingsViewer));
+            }
+        }
+    }
+
+    public class HamOption
+    {
+
+        public string Title { get; set; }
+
+        public Type PageType { get; set; }
+
+        public string IconFile { get; set; }
+
+
     }
 }
