4579f6cf88c80bfd5912564ad30d938ebfba5720
Steven <yourgamesbeover@gmail.com>
2016-11-21 16:46:22 -0500

    Added in basic unit tests for networking

---
+++ UnitTests/ServerConnectionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Countdown.Networking;
+using Countdown.Networking.Results;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ServerConnectionTests {
+        private const string ServerUrl = "http://129.22.62.229:5000";
+        private const string Username = "user_1";
+        private const string Password = "password_1";
+
+        [TestMethod]
+        public async System.Threading.Tasks.Task TestLogIn()
+        {
+            var con = new ServerConnection();
+            Assert.IsFalse(con.IsConnected, "ServerConnection not initially disconnected");
+            con.Connect(ServerUrl);
+            Assert.IsTrue(con.IsConnected, "ServerConnection not not connected after calling Connect");
+            var response = await con.LogIn(new UserAuth {Username = Username, Password = Password});
+            Assert.AreEqual(LoginResult.Success, response, "LogIn did not return LoginResult.Success");
+        }
+    }
+}
